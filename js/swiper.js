6f75f5120bc261fd79d8d3c7036cffd15a4203ce
EriyaDev <eriyagamedev@gmail.com>
2025-08-30 21:55:20 +0800

    w grid, aman

---
+++ js/swiper.js
@@ -0,0 +1,29 @@
+var manySwiper = new Swiper(".many-swiper", {
+  slidesPerView: 1.1,
+  spaceBetween: 30,
+  grabCursor: true,
+  pagination: {
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1.2,
+      spaceBetween: 30,
+    },
+    768: {
+      slidesPerView: 1.6,
+      spaceBetween: 30,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    1280: {
+      slidesPerView: 2.5,
+      spaceBetween: 30,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
